6032bcf3cd1e17cda1858475ce0108951edab765
rcd <rcdcim@gmail.com>
2026-04-17 14:32:37 -0300

    improve

--- offline/criar-eagle.js
+++ offline/criar-eagle.js
@@ -1,7 +1,7 @@
 import { confirm } from '@inquirer/prompts'
 import eagle from '../src/eagle.js'
 
-const answer = await confirm({ message: 'Continuar?', default: false })
+const answer = await confirm({ message: 'Continuar??', default: false })
 
 if (answer) {
   await eagle.sincronizar()
